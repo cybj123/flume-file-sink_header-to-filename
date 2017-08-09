ee3ecd2cacbeee78c451d53cd2cb6fc2e3766789
tony <tony@DESKTOP-APNK30K>
2017-08-09 14:18:19 +0800

    1111

---
+++ src/main/java/flume/mysink/InputNotSpecifiedException.java
@@ -0,0 +1,23 @@
+package flume.mysink;
+
+/**
+ * @author zhangxu
+ */
+public class InputNotSpecifiedException extends RuntimeException {
+    private static final long serialVersionUID = 1102327497549834945L;
+
+    public InputNotSpecifiedException() {
+    }
+
+    public InputNotSpecifiedException(String message) {
+        super(message);
+    }
+
+    public InputNotSpecifiedException(String message, Throwable t) {
+        super(message, t);
+    }
+
+    public InputNotSpecifiedException(Throwable t) {
+        super(t);
+    }
+}
